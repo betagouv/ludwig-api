7aa095bb65d1f1ebcd4b100327cdd36a78c9ba7e
Matti Schneider <matti.schneider@data.gouv.fr>
2015-03-26 11:05:00 +0100

    Refactor fixture loading
    Return a function
    Log errors
    Take `force`, `source` and `models` parameters

--- fixtures/index.js
+++ fixtures/index.js
@@ -1,26 +1,57 @@
 var path = require('path'),
-    fs   = require('fs');
+    fs   = require('fs'),
+    _    = require('lodash');
 
-var mongoose = require('../lib/config/mongoose').instance();
 
+/** Load all fixtures from the given folder if the matching collection is empty.
+*
+*@param  {Object}    [options]    May contain:
+*     -  {Mongoose}       [mongoose = connect(process.env.MONGODB_URL)]   The Mongoose connection to use to load fixtures to.
+*     -  {String}         [source = dirname]  Path to the directory containing model-named JS or JSON files defining document attributes to insert.
+*     -  {Array<String>}  [models = _all_]  The subset of models to load fixtures for.
+*     -  {Boolean}        [force = false]   Always load the fixtures, even if the collection is not empty.
+*/
+exports.loadFixtures = function (options) {
+    var config = _.clone(options);
 
-fs.readdirSync(__dirname).forEach(function (filename) {
-    if (! filename.match(/\.js(on)?$/) || filename === 'index.js') {
-        return;
+    if (! config.mongoose) {
+        config.mongoose = require('mongoose');
+        config.mongoose.connect(process.env.MONGODB_URL);
+        require('../lib/config/mongoose').init(config.mongoose);
     }
 
-    var data = require('./' + filename),
-        modelName = path.basename(filename, path.extname(filename));
+    _.defaults(config, {
+        source: __dirname,
+        models: [],
+        force : false
+    });
 
-    var model = mongoose.model(modelName);
+    return loadFixtures(config);
+}
 
-    model.findOne(function (err, document) {
-        if (document) {
-            return;
-        }
 
-        model.create(data).then(function (created) {
-            console.log('Loaded', modelName, 'fixture from', filename, ': ', created);
+/**
+*@private
+*/
+function loadFixtures(config) {
+    fs.readdirSync(config.source).forEach(function (filename) {
+        if (! filename.match(/\.js(on)?$/) || filename === 'index.js') return;
+
+        var modelName = path.basename(filename, path.extname(filename));
+
+        if (config.models.length && ! _.contains(config.models, modelName)) return;
+
+        var data = require('./' + filename),
+            model = config.mongoose.model(modelName);
+
+        model.findOne(function (err, document) {
+            if (! config.force && document) return;
+
+            model.create(data).then(function (created) {
+                console.log('Loaded', modelName, 'fixture from', filename, ': ', created);
+            }, function (error) {
+                console.error('Could not load', modelName, 'fixture from', filename, ': ', error);
+            }).end();
         });
     });
-});
+}
