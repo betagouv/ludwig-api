96699b54d58e24d3655387d1d6174394123f5151
fpagnoux <fpagnoux@octo.com>
2015-07-22 16:48:39 +0200

    Migration script to initiate resultUpdated for all tests

---
+++ migrations/setResultUpdatedDate.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/dds');
+
+require('../lib/config/mongoose').init(mongoose);
+require('../lib/models/acceptanceTest');
+
+var AcceptanceTest = mongoose.model('AcceptanceTest');
+
+var AcceptanceTestActivity = mongoose.model('AcceptanceTestActivity');
+
+var stream = AcceptanceTest.find().stream();
+
+stream.on('data', function(acceptanceTest) {
+
+    AcceptanceTestActivity
+        .findOne({
+            type: 'results_update',
+            acceptanceTest: acceptanceTest._id
+        }, {}, {
+            sort: '-date'
+        }, function(err, activity) {
+            if (err) return console.trace(err);
+            if (!activity) return ;
+            acceptanceTest
+                .set('resultUpdated', activity.date)
+                .save(function(err, data) {
+                    if (err) return console.trace(err);
+                    console.log(acceptanceTest._id + ' migrated');
+                    console.log(data)
+                    console.log(activity.date)
+                });
+        });
+
+});
+
+stream.on('end', function() {
+    console.log('completed!');
+    //process.exit();
+});
+
+stream.on('error', function(err) {
+    console.trace(err);
+});
+
